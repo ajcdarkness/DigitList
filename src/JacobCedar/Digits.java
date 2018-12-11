5584f709a04235334e5e622963859efac7bd7168
jacob cedar <jcedar8640@bths.edu>
2018-12-11 11:42:34 -0500

    added stuff

--- src/JacobCedar/Digits.java
+++ src/JacobCedar/Digits.java
@@ -6,6 +6,23 @@ public class Digits{
     private ArrayList<Integer> digitList;
     public Digits(int num){
         ArrayList list = new ArrayList();
-        String x = num.toString();
+        String x = Integer.toString(num);
+        for(int i = 0; i < x.length(); i++){
+            int y= Integer.parseInt(x.substring(i,i+1));
+            list.add(y);
+        }
+    }
+
+    public ArrayList<Integer> getDigitList() {
+        return digitList;
+    }
+
+    public boolean isStrictlyIncreasing(){
+        for(int i = 0; i < digitList.size()-1; i++){
+            if(digitList.get(i).intValue() >= digitList.get(i+1).intValue()){
+                return false;
+            }
+        }
+        return true;
     }
 }

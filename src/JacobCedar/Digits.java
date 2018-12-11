0d85c1c9295b973bbe6444348066830e657df0a0
BT_1N3_16@bktech.local <BT_1N3_16@bktech.local>
2018-12-11 11:27:20 -0500

    Initial commit

---
+++ src/JacobCedar/Digits.java
@@ -0,0 +1,11 @@
+package JacobCedar;
+
+import java.util.*;
+import java.lang.String;
+public class Digits{
+    private ArrayList<Integer> digitList;
+    public Digits(int num){
+        ArrayList list = new ArrayList();
+        String x = num.toString();
+    }
+}

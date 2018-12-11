322656194f0a73d9e8202cc37c276b269c35d5ce
jacob cedar <jcedar8640@bths.edu>
2018-12-11 11:46:57 -0500

    added stuff

--- src/JacobCedar/Digits.java
+++ src/JacobCedar/Digits.java
@@ -3,13 +3,17 @@ package JacobCedar;
 import java.util.*;
 import java.lang.String;
 public class Digits{
-    private ArrayList<Integer> digitList;
+    private ArrayList<Integer> digitList = new ArrayList<Integer>();
     public Digits(int num){
-        ArrayList list = new ArrayList();
-        String x = Integer.toString(num);
-        for(int i = 0; i < x.length(); i++){
-            int y= Integer.parseInt(x.substring(i,i+1));
-            list.add(y);
+        digitList = new ArrayList<Integer>();
+
+        if(num == 0){
+            digitList.add(new Integer(0));
+
+        }
+        while(num > 0){
+            digitList.add(0, new Integer(num%10));
+            num /= 10;
         }
     }
 
